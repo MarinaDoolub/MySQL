525804eebc98970ae34830acd834acf015224671
MarinaDoolub <marinaluna1504@gmail.com>
2025-06-06 14:00:01 +0400

    Add files via upload

---
+++ insertTableAuthors.sql
@@ -0,0 +1,8 @@
+INSERT INTO Authors
+VALUES(1,"George","Orwell"),
+      (2,"Jane","Austen"),
+      (3,"Chinua","Achebe"),
+      (4,"Joanne","Rowling"),
+      (5,"Nicholas","Sparks");
+
+SELECT * FROM Authors;
