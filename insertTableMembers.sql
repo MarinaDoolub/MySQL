ce8f0714b911e5efb6b4433c75adbe9db56747eb
MarinaDoolub <marinaluna1504@gmail.com>
2025-06-08 09:43:37 +0400

    Add files via upload

---
+++ insertTableMembers.sql
@@ -0,0 +1,8 @@
+INSERT INTO Members
+VALUES(201,"Alice","Smith","alice34@gmail.com","2025-01-06"),
+      (202,"Bob","Johnson","bobJJ5@gmail.com","2025-01-05"),
+      (203,"Jamie","Clover","CloverJamie@gmail.com","2025-01-04"),
+      (204,"Ophelia","Afflie","ophel636@gmail.com","2025-01-09"),
+      (205,"Mylin","Kong","mylinkong@gmail.com","2025-01-07");
+
+SELECT * FROM Members;
