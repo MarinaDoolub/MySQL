d68d423fda8663b6cc9477f115f4dd62b7284588
MarinaDoolub <marinaluna1504@gmail.com>
2025-06-08 09:43:07 +0400

    Add files via upload

---
+++ createTableMembers.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Members(
+                     member_id int,
+                     first_name varchar(50),
+                     last_name varchar(50),
+                     email varchar(50),
+                     join_date date
+);
