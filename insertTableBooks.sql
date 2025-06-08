887b32b35567d7b695d547846da21fec67ede064
MarinaDoolub <marinaluna1504@gmail.com>
2025-06-08 09:42:14 +0400

    Add files via upload

---
+++ insertTableBooks.sql
@@ -0,0 +1,10 @@
+INSERT INTO Books
+VALUES(101,"Nineteen Eighty-Four",1,"Science Fiction",1949,True),
+      (102,"Pride and Prejudice",2,"Romance",1813,False),
+      (103,"Things Fall Apart",3,"Historical Fiction",1958,False),
+      (104,"Harry Potter and the Sorcerer's Stone ",4,"Fantasy",1997 ,True),
+      (105,"The Notebook",5,"Romance",1996,True);
+
+
+
+SELECT * FROM Books;
