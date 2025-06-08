d5a237d745080563c56d9782bcf2e28db1c69115
MarinaDoolub <marinaluna1504@gmail.com>
2025-06-08 09:41:42 +0400

    Add files via upload

---
+++ createTableBooks.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Books(
+				   book_id int,
+                   title varchar(50),
+                   author_id int,
+                   genre varchar(25),
+                   published_year int,
+                   available bool
+);
