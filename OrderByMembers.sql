9917ede95311fc619fac9bbb3957e5634f17d5ed
MarinaDoolub <marinaluna1504@gmail.com>
2025-06-16 09:36:35 +0400

    Order By

---
+++ OrderByMembers.sql
@@ -0,0 +1,6 @@
+-- This query shows all members from the Members table
+-- It sorts the results by last name, from Z to A (descending order)
+-- The ORDER BY keyword is used to sort
+-- DESC means descending (reverse) order
+SELECT * FROM Members
+ORDER BY last_name DESC;
